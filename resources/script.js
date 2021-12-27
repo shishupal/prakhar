ab38b049bceeb7b0d1e833dd03df319948914967
Prakhar gupta <prakhar@Prakhars-MacBook-Pro.local>
2021-12-26 20:14:14 -0700

    refactoring + grandsome on each td

--- resources/script.js
+++ resources/script.js
@@ -1,33 +1,40 @@
 function addItem() {
   var counter = $("#myTable tr").length;
 
-  var lastitemappend = `<tr class="lastItem"><td></td><td></td><td></td><td></td><td>भाड़ा</td><td><input type="text" id="fare" class="form-control" /></td><td></td></tr>
-    <tr class="table table-success">
-  <th></th><th></th><th>कुल वज़न</th><th><input type="text" id="netWeight" class="form-control" disabled/></th>
-  <th>कुल राशि</th><th><input type="text" id="netAmount" class="form-control netAmount" disabled/></th><th></th>
-</tr>`;
+  var lastitemappend = `<tr class="lastItem">
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>भाड़ा</td>
+                            <td><input type="text" id="fare" class="form-control" /></td>
+                            <td></td>
+                        </tr>
+                        <tr class="table table-success">
+                            <th></th>
+                            <th></th>
+                            <th>कुल वज़न</th>
+                            <th><input type="text" id="netWeight" class="form-control" disabled/></th>
+                            <th>कुल राशि</th><th><input type="text" id="netAmount" class="form-control netAmount" disabled/></th>
+                            <th></th>
+                        </tr>`;
 
+  var cell1 = `<tr class=${"insertafter"+counter}>
+                 <td><div class="autocomplete" style="width:300px;"><input id="myInput" type="text" name="myCountry" placeholder="आइटम"></div></td>
+                 <td> <input type="text" id=${"size"+counter} class="form-control"/></td>
+                 <td> <input type="text" id=${"quantity"+counter} class="quantity" data-name=${"dtname"+counter} class="form-control round"/></td>
+                 <td> <input type="text" id=${"weight"+counter}  class="weight" data-name=${"dtname"+counter} class="form-control round"/></td>
+                 <td> <input type="text" id=${"rate"+counter} class="rate" data-name=${"dtname"+counter} class="form-control round"/></td>
+                 <td class="totalcol"> <input type="text" id=${"total"+counter}  class="total" data-name=${"dtname"+counter} class="form-control round" disabled/></td>
+                 <td class="print"><button type=\"button\"  class=\"btn btn-danger delItemrow\" data-name=${counter}>Delete Item</button></td> 
+               </tr>`;
 
-
-  var cell1 = `<tr class=${"insertafter"+counter}><td>
- <div class="autocomplete" style="width:300px;">
- <input id="myInput" type="text" name="myCountry" placeholder="आइटम"></div></td>
- <td> <input type="text" id=${"size"+counter} class="form-control"/></td><td> <input type="text" id=${"quantity"+counter} class="quantity" data-name=${"dtname"+counter} class="form-control round"/></td>
-<td> <input type="text" id=${"weight"+counter}  class="weight" data-name=${"dtname"+counter} class="form-control round"/></td>
- <td> <input type="text" id=${"rate"+counter} class="rate" data-name=${"dtname"+counter} class="form-control round"/></td>
-  <td class="totalcol"> <input type="text" id=${"total"+counter}  class="total" data-name=${"dtname"+counter} class="form-control round" disabled/></td><td class="print">
- <button type=\"button\"  class=\"btn btn-danger delItemrow\" data-name=${counter}>Delete Item</button></td> </tr>`;
-
-  //$("#myTable tbody").append(cell1).append(lastitemappend);
   $(".lastItem,.table-success").remove();
   $("#myTable tbody").append(cell1).append(lastitemappend);
   $(".additem").attr("disabled", true);
   if ($("#myTable tbody tr").length === 2) {
-
     $(".additem").removeAttr('disabled');
   }
-
-
 
   var countries = ["Angle",
     "Angle 1x1",
@@ -133,108 +140,7 @@ function netEstimate() {
   });
 }
 
-function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
-  var currentFocus;
-  /*execute a function when someone writes in the text field:*/
-  var inp = inp;
-  inp.addEventListener("input", function (e) {
-    var a, b, i, val = this.value;
-    /*close any already open lists of autocompleted values*/
-    closeAllLists();
-    if (!val) {
-      return false;
-    }
-    currentFocus = -1;
-    /*create a DIV element that will contain the items (values):*/
-    a = document.createElement("DIV");
-    a.setAttribute("id", this.id + "autocomplete-list");
-    a.setAttribute("class", "autocomplete-items");
-    /*append the DIV element as a child of the autocomplete container:*/
-    this.parentNode.appendChild(a);
-    /*for each item in the array...*/
-    for (i = 0; i < arr.length; i++) {
-      /*check if the item starts with the same letters as the text field value:*/
-      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-        /*create a DIV element for each matching element:*/
-        b = document.createElement("DIV");
-        /*make the matching letters bold:*/
-        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-        b.innerHTML += arr[i].substr(val.length);
-        /*insert a input field that will hold the current array item's value:*/
-        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-        /*execute a function when someone clicks on the item value (DIV element):*/
-        b.addEventListener("click", function (e) {
-          /*insert the value for the autocomplete text field:*/
-          inp.value = this.getElementsByTagName("input")[0].value;
-          /*close the list of autocompleted values,
-          (or any other open lists of autocompleted values:*/
-          closeAllLists();
-        });
-        a.appendChild(b);
-      }
-    }
-  });
-  /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function (e) {
-    var x = document.getElementById(this.id + "autocomplete-list");
-    if (x) x = x.getElementsByTagName("div");
-    if (e.keyCode == 40) {
-      /*If the arrow DOWN key is pressed,
-      increase the currentFocus variable:*/
-      currentFocus++;
-      /*and and make the current item more visible:*/
-      addActive(x);
-    } else if (e.keyCode == 38) { //up
-      /*If the arrow UP key is pressed,
-      decrease the currentFocus variable:*/
-      currentFocus--;
-      /*and and make the current item more visible:*/
-      addActive(x);
-    } else if (e.keyCode == 13) {
-      /*If the ENTER key is pressed, prevent the form from being submitted,*/
-      e.preventDefault();
-      if (currentFocus > -1) {
-        /*and simulate a click on the "active" item:*/
-        if (x) x[currentFocus].click();
-      }
-    }
-  });
 
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
-  }
-  /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-  });
-}
 
 $(document).ready(function () {
   var weight = 0;
@@ -255,7 +161,6 @@ $(document).ready(function () {
       $(`${"#myTable td input#total"+weightnumber}`).val(total.toFixed(2));
       grandTotal[weightnumber] = total.toFixed(2);
       console.log(grandTotal)
-
     }
 
 
@@ -264,6 +169,7 @@ $(document).ready(function () {
     }
 
     e.preventDefault();
+
   });
 
     /// in rate  opertaion
@@ -277,18 +183,16 @@ $(document).ready(function () {
       $(`${"#myTable td input#total"+ratenumber}`).val(total.toFixed(2));
       grandTotal[ratenumber] = total.toFixed(2);
       console.log(grandTotal);
-
     }
 
     if ($(`${"#myTable td input#total"+ratenumber}`).val() > 0) {
       $(".additem").removeAttr('disabled');
     }
 
-
     e.preventDefault();
   });
 
-// Grand total click
+  // Grand total click
   $(document.body).on('click', ".grandTotalAmt", function (e) {
     var grandTotalAmt1 = grandTotalAmt(grandTotal) + Number($('#fare').val())
     $("#netAmount").val(grandTotalAmt1.toFixed(2));
@@ -299,8 +203,6 @@ $(document).ready(function () {
   });
 
 
-  
-  
   // delte Row
   $(document.body).on('click', ".delItemrow", function (e) {
 
@@ -316,13 +218,11 @@ $(document).ready(function () {
     var totalwight=  grandTotalwight(totalweightitem);
     $("#netWeight").val(totalwight.toFixed(2));
 
-
-
     e.preventDefault();
   });
 
  // delte Row
- $(document.body).on('blur', "#fare", function (e) {
+ $(document.body).on('blur', "#fare, table td", function (e) {
 
   var grandTotalAmt1 = grandTotalAmt(grandTotal);
   
